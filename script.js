70cd5096fd67156215a54a156ffd9663efa133b7
Plasnid <stuart@sackler.ca>
2020-11-27 00:03:01 -0500

    questions added

---
+++ script.js
@@ -0,0 +1,62 @@
+/**
+ * ! Answer all questions in this file only.  Do not directly edit the index.html file
+ * ! Please bug check all your work before turning in the test.
+ * ! Each section of the test is worth 25%
+ * 
+ * ! Question 1: Objects
+ * * Create an object about your favourite food containing the following:
+ * * -the name of the dish(string)
+ * * -the calories in the dish(number)
+ * * -an array of the ingredients in the food
+ * * -is it spicy(boolan)
+ * * -a getter for finding the spicy value of the food
+ * * -a setter for switching the spicy value of the food
+ * * -a function that will loop through and console log each ingredient in ingredients array
+ * *
+ * * Please run the getter, setter, and looping function once you have made the object
+ */
+
+/**
+ * ! Question 2: Functions
+ * * Create a single line fat arrow function that will take in a name and a number
+ * * Make the fat arrow function return a template literal that will return the following phrase
+ * * "My name is _nameParameter_ and I would like _numberParameter_ burgers"
+ */
+
+/**
+ * ! Question 3a: ES6 Class
+ * * Create a es6 class representing a stick
+ * * Send the class a type of wood and a length
+ * * Add a function called from a tree that will log the following phrase 
+ * * "The stick is made out of _woodType_ and is _lengthParameter_ long"
+ */
+
+/**
+ * ! Question 3b: extending ES6 class
+ * * Extend the stick class into a sports item(baseball bat, lacrosse stick, hockey stick, bow)
+ * * Add the following parameters
+ * * -type of sport
+ * * -season of use
+ * * -manufacturer
+ * *
+ * * Add the following function to your class
+ * * -create a function that will use dom manipulation to
+ * *    -create a ul tag and add it to the html
+ * *    -create an li for each parameter and set its inner text to that parameter
+ * *    -add the li to the screen
+ */
+
+/**
+ * ! Question 4: Fetch and Display
+ * * Create a json file that contains 2 arrays
+ * * 1 array of 4 different commands you would give to a pet puppy
+ * * 1 array of 5 different reactions your puppy would respond with
+ * 
+ * * -fetch the json file
+ * * -on fetch the data use dom manipulation to add an H2 tag to the screen
+ * * with the text "play time"
+ * * -add a click event listener to the h2 from this javascript file
+ * * -when the h2 is clicked write a p tag with 1 piece of text from each of the 2 arrays to the screen(total 2 strings, 1 from each array)
+ * 
+ * * -Do not use a class to answer this question
+ */
